cb3db7db2a083e45741c3e9399c6c04701f4982f
meledych1111-ux <meledych1111@gmail.com>
2025-10-23 18:41:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,55 @@
-// AltFlow entry point (ES-module edition)
+// js/main.js  (строго последовательная загрузка)
 window.BrushLibrary = { brushes: {} };
 
-const coreModules = [
-  './core/EventManager.js',
-  './core/BrushBase.js',
-  './core/ToolManager.js',
-  './core/LayerManager.js',
-  './core/FilterManager.js',
-  './core/CanvasManager.js',
-  './core/App.js'
-];
-
-const brushModules = [
-  './brushes/ArtistBrushes.js',
-  './brushes/MetalBrush.js',
-  './brushes/GlowBrush.js',
-  './brushes/Sphere3DBrush.js',
-  './brushes/GlassBrush.js',
-  './brushes/CeramicBrush.js',
-  './brushes/TextureBrush.js',
-  './brushes/AnimeBrushes.js',
-  './brushes/DrawingBrushes.js'
-];
+async function loadModule(path) {
+  await import(path);   // ждём, пока модуль выполнится
+}
 
 (async () => {
-  // 1. загружаем ядро
-  for (const path of coreModules) await import(path);
+  try {
+    // 1. ЯДРО (важно до кистей!)
+    await loadModule('./core/EventManager.js');
+    await loadModule('./core/BrushBase.js');
+    await loadModule('./core/ToolManager.js');
+    await loadModule('./core/LayerManager.js');
+    await loadModule('./core/FilterManager.js');
+    await loadModule('./core/CanvasManager.js');
+    await loadModule('./core/App.js');
 
-  // 2. загружаем кисти (глобальные классы)
-  for (const path of brushModules) await import(path);
+    // 2. КИСТИ (теперь BrushBase уже доступен)
+    await loadModule('./brushes/ArtistBrushes.js');
+    await loadModule('./brushes/MetalBrush.js');
+    await loadModule('./brushes/GlowBrush.js');
+    await loadModule('./brushes/Sphere3DBrush.js');
+    await loadModule('./brushes/GlassBrush.js');
+    await loadModule('./brushes/CeramicBrush.js');
+    await loadModule('./brushes/TextureBrush.js');
+    await loadModule('./brushes/AnimeBrushes.js');
+    await loadModule('./brushes/DrawingBrushes.js');
 
-  // 3. собираем классы кистей в библиотеку
-  const brushClasses = [
-    OilPaintBrush, WatercolorBrush, PastelBrush, CharcoalBrush, InkBrush, DigitalArtBrush,
-    MetalBrush, GlowBrush, Sphere3DBrush, GlassBrush, CeramicBrush, TextureBrush,
-    AnimeHairBrush, AnimeEyeBrush, AnimeLipBrush, AnimeSkinBrush, AnimeEyebrowBrush,
-    TechnicalPenBrush, CircleTemplateBrush, RectangleTemplateBrush, PolygonTemplateBrush, LineTemplateBrush
-  ];
+    // 3. Регистрируем кисти
+    const brushClasses = [
+      OilPaintBrush, WatercolorBrush, PastelBrush, CharcoalBrush, InkBrush, DigitalArtBrush,
+      MetalBrush, GlowBrush, Sphere3DBrush, GlassBrush, CeramicBrush, TextureBrush,
+      AnimeHairBrush, AnimeEyeBrush, AnimeLipBrush, AnimeSkinBrush, AnimeEyebrowBrush,
+      TechnicalPenBrush, CircleTemplateBrush, RectangleTemplateBrush, PolygonTemplateBrush, LineTemplateBrush
+    ];
 
-  for (const Cl of brushClasses) {
-    const key = Cl.name.replace(/Brush$/, '').toLowerCase();
-    BrushLibrary.brushes[key] = Cl;
+    for (const Cl of brushClasses) {
+      const key = Cl.name.replace(/Brush$/, '').toLowerCase();
+      BrushLibrary.brushes[key] = Cl;
+    }
+
+    // 4. Запускаем приложение
+    if (window.App) {
+      window.app = new App();
+      window.app.init();
+    }
+
+    console.log('AltFlow загружен и проверен');
+    console.log('Brushes:', Object.keys(BrushLibrary.brushes));
+
+  } catch (err) {
+    console.error('Ошибка загрузки модулей:', err);
   }
-
-  // 4. запускаем приложение (App.js должен создать window.app)
-  if (window.App) {
-    window.app = new App();
-    window.app.init();
-  }
-
-  console.log('AltFlow загружен и проверен');
-  console.log('Brushes:', Object.keys(BrushLibrary.brushes));
 })();
